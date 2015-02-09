8d0668acff9c4e919b9883113538e9baad178349
Bematech <robert.ditusa@bematech.com>
2015-02-09 16:52:51 -0500

    USING DIFFERENT COMMAND SET FOR DRAWER

--- BemaPos/src/com/kaching123/pos/drawer/OpenDrawerAction.java
+++ BemaPos/src/com/kaching123/pos/drawer/OpenDrawerAction.java
@@ -10,7 +10,9 @@ import com.kaching123.pos.Action;
  */
 public class OpenDrawerAction extends Action {
 
-	private static final byte[] COMMAND_BYTES = new byte[]{DLE, 0x14, 0x01, 0x00, 0x04};
+	//private static final byte[] COMMAND_BYTES = new byte[]{DLE, 0x14, 0x01, 0x00, 0x04};
+    private static final byte[] COMMAND_BYTES = new byte[]{ESC, 0x70, 0x00, 0x33, 0x33};
+    //"<1B><70><00><33><33>"
 	
 	@Override
 	public byte[] getCommand() {
