80a7b8024072be5e6166b85429e47b75663ebcff
Terry <TELIYIN@GMAIL.COM>
2015-07-02 16:36:20 -0400

    test commit.

--- TabletCR/src/main/java/com/kaching123/tcr/reports/XReportQuery.java
+++ TabletCR/src/main/java/com/kaching123/tcr/reports/XReportQuery.java
@@ -126,7 +126,7 @@ public final class XReportQuery {
         BigDecimal check = BigDecimal.ZERO;
         BigDecimal ebtCash = BigDecimal.ZERO;
         BigDecimal ebtFoodstamp = BigDecimal.ZERO;
-        BigDecimal debit = BigDecimal.ZERO;
+        BigDecimal debit = BigDecimal.ONE;
 
         HashMap<String, BigDecimal> cards = new HashMap<String, BigDecimal>();
         while (c.moveToNext()) {
