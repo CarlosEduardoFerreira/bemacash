c33a54309f464f63f158884ffce198c2f22b2b83
longjiao2015 <long.jiao@bematechus.com>
2016-05-20 16:12:29 -0400

    update tax group decimal digit to 3.

--- TabletCR/src/main/java/com/kaching123/tcr/component/CurrencyTextWatcher.java
+++ TabletCR/src/main/java/com/kaching123/tcr/component/CurrencyTextWatcher.java
@@ -65,7 +65,7 @@ public class CurrencyTextWatcher implements TextWatcher {
                 setSelection();
             } else if (isDecimalPart) {
                 final String decimalPart = value.substring(value.indexOf(DECIMAL_SEPARATOR) + 1);
-                if (decimalPart.length() > 2) {
+                if (decimalPart.length() > 3) {
                     String intPart = value.replaceAll("\\,", "");
                     intPart = intPart.substring(0, intPart.indexOf(DECIMAL_SEPARATOR) + 3);
                     final String priceFormattedString = UiHelper.priceFormat(UiHelper.parseBrandDecimalInput(intPart));
