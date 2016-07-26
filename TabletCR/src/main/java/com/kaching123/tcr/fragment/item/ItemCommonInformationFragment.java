703f063ba19a1525a119fd0fcceeeb412c32bbc5
vkompaniets <vladimir.kompaniets@dataart.com>
2016-07-26 19:00:19 +0300

    BEMA-886 Inventory - progress

--- TabletCR/src/main/java/com/kaching123/tcr/fragment/item/ItemCommonInformationFragment.java
+++ TabletCR/src/main/java/com/kaching123/tcr/fragment/item/ItemCommonInformationFragment.java
@@ -1,32 +1,68 @@
 package com.kaching123.tcr.fragment.item;
 
+import android.content.Context;
+import android.database.Cursor;
+import android.database.MatrixCursor;
+import android.database.MergeCursor;
+import android.os.Bundle;
+import android.support.v4.app.LoaderManager.LoaderCallbacks;
+import android.support.v4.content.Loader;
+import android.view.View;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
+import com.getbase.android.db.loaders.CursorLoaderBuilder;
 import com.kaching123.tcr.R;
+import com.kaching123.tcr.adapter.SpinnerAdapter;
+import com.kaching123.tcr.fragment.UiHelper;
 import com.kaching123.tcr.model.ItemModel;
+import com.kaching123.tcr.store.ShopProvider;
+import com.kaching123.tcr.store.ShopStore.CategoryTable;
+import com.kaching123.tcr.store.ShopStore.DepartmentTable;
+import com.kaching123.tcr.store.ShopStore.TaxGroupTable;
 
 import org.androidannotations.annotations.EFragment;
+import org.androidannotations.annotations.ItemSelect;
 import org.androidannotations.annotations.ViewById;
 
 import static com.kaching123.tcr.fragment.UiHelper.showPrice;
+import static com.kaching123.tcr.model.ContentValuesUtil._decimal;
 
 /**
  * Created by vkompaniets on 21.07.2016.
  */
 @EFragment(R.layout.item_common_information_fragment)
-public class ItemCommonInformationFragment extends ItemBaseFragment {
+public class ItemCommonInformationFragment extends ItemBaseFragment implements LoaderCallbacks<Cursor> {
 
     @ViewById EditText description;
     @ViewById EditText salesPrice;
     @ViewById Spinner department;
     @ViewById Spinner category;
+    @ViewById Spinner taxGroup;
     @ViewById CheckBox activeStatus;
+
+    private static final int DEPARTMENT_LOADER_ID = 0;
+    private static final int CATEGORY_LOADER_ID = 1;
+    private static final int TAX_GROUP_LOADER_ID = 2;
+
+    protected DepartmentSpinnerAdapter departmentAdapter;
+    protected CategorySpinnerAdapter categoryAdapter;
+    protected TaxGroupSpinnerAdapter taxGroupAdapter;
 
     @Override
     protected void setViews() {
+        departmentAdapter = new DepartmentSpinnerAdapter(getActivity());
+        department.setAdapter(departmentAdapter);
 
+        categoryAdapter = new CategorySpinnerAdapter(getActivity());
+        category.setAdapter(categoryAdapter);
+
+        taxGroupAdapter = new TaxGroupSpinnerAdapter(getActivity());
+        taxGroup.setAdapter(taxGroupAdapter);
+
+        initLoaders();
     }
 
     @Override
@@ -36,5 +72,141 @@ public class ItemCommonInformationFragment extends ItemBaseFragment {
         showPrice(salesPrice, model.price);
         activeStatus.setChecked(model.isActiveStatus);
 
+    }
+
+    private void initLoaders() {
+        getLoaderManager().initLoader(DEPARTMENT_LOADER_ID, null, this);
+        getLoaderManager().initLoader(CATEGORY_LOADER_ID, null, this);
+        getLoaderManager().initLoader(TAX_GROUP_LOADER_ID, null, this);
+    }
+
+    @ItemSelect
+    protected void departmentItemSelected(boolean selected, int position){
+        getModel().departmentGuid = departmentAdapter.getGuid(position);
+        getLoaderManager().restartLoader(CATEGORY_LOADER_ID, null, this);
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        switch (id) {
+            case DEPARTMENT_LOADER_ID:
+                return CursorLoaderBuilder.forUri(ShopProvider.contentUri(DepartmentTable.URI_CONTENT))
+                        .projection(new String[]{DepartmentTable.ID, DepartmentTable.TITLE, DepartmentTable.GUID})
+                        .build(getActivity());
+            case CATEGORY_LOADER_ID:
+                return CursorLoaderBuilder.forUri(ShopProvider.contentUri(CategoryTable.URI_CONTENT))
+                        .projection(new String[]{CategoryTable.ID, CategoryTable.TITLE, CategoryTable.GUID})
+                        .where(CategoryTable.DEPARTMENT_GUID + " = ?", getModel().departmentGuid)
+                        .build(getActivity());
+            case TAX_GROUP_LOADER_ID:
+                return CursorLoaderBuilder.forUri(ShopProvider.contentUri(TaxGroupTable.URI_CONTENT))
+                    .projection(new String[]{TaxGroupTable.ID, TaxGroupTable.GUID, TaxGroupTable.TITLE, TaxGroupTable.TAX})
+                    .build(getActivity());
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        switch (loader.getId()){
+            case DEPARTMENT_LOADER_ID:
+                departmentAdapter.changeCursor(data);
+                break;
+            case CATEGORY_LOADER_ID:
+                categoryAdapter.changeCursor(data);
+                break;
+            case TAX_GROUP_LOADER_ID:
+                taxGroupAdapter.changeCursor(data);
+                break;
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        switch (loader.getId()) {
+            case DEPARTMENT_LOADER_ID:
+                departmentAdapter.changeCursor(null);
+                break;
+            case CATEGORY_LOADER_ID:
+                categoryAdapter.changeCursor(null);
+                break;
+            case TAX_GROUP_LOADER_ID:
+                taxGroupAdapter.changeCursor(null);
+                break;
+        }
+    }
+
+    public static class CategorySpinnerAdapter extends SpinnerAdapter {
+
+        public CategorySpinnerAdapter(Context context) {
+            super(context,
+                    R.layout.spinner_item_light,
+                    new String[]{CategoryTable.TITLE},
+                    new int[]{android.R.id.text1},
+                    R.layout.spinner_dropdown_item);
+        }
+
+        @Override
+        protected String getIdColumnName() {
+            return CategoryTable.GUID;
+        }
+    }
+
+    public static class DepartmentSpinnerAdapter extends SpinnerAdapter {
+
+        public DepartmentSpinnerAdapter(Context context) {
+            super(context,
+                    R.layout.spinner_item_light,
+                    new String[]{DepartmentTable.TITLE},
+                    new int[]{android.R.id.text1},
+                    R.layout.spinner_dropdown_item);
+        }
+
+        @Override
+        protected String getIdColumnName() {
+            return DepartmentTable.GUID;
+        }
+    }
+
+    public class TaxGroupSpinnerAdapter extends SpinnerAdapter {
+
+        public TaxGroupSpinnerAdapter(Context context) {
+            super(context,
+                    R.layout.spinner_item_light,
+                    new String[]{TaxGroupTable.TITLE},
+                    new int[]{android.R.id.text1},
+                    R.layout.spinner_dropdown_item);
+        }
+
+        @Override
+        public void bindView(View view, Context context, Cursor c) {
+            ((TextView) view.findViewById(android.R.id.text1)).setText(
+                    String.format("%s (%s)",
+                            c.getString(c.getColumnIndex(TaxGroupTable.TITLE)),
+                            UiHelper.percentFormat(_decimal(c, c.getColumnIndex(TaxGroupTable.TAX)))
+                    )
+            );
+        }
+
+        @Override
+        public void changeCursor(Cursor cursor) {
+            if (cursor == null) {
+                super.changeCursor(cursor);
+                return;
+            }
+
+            MatrixCursor extras = new MatrixCursor(new String[]{TaxGroupTable.ID, TaxGroupTable.GUID, TaxGroupTable.TITLE, TaxGroupTable.TAX});
+            extras.addRow(new String[]{"0", null, getString(R.string.item_tax_group_default), _decimal(getApp().getShopInfo().taxVat)});
+            Cursor[] cursors = {extras, cursor};
+            Cursor extendedCursor = new MergeCursor(cursors);
+
+            super.changeCursor(extendedCursor);
+        }
+
+        @Override
+        protected String getIdColumnName() {
+            return TaxGroupTable.GUID;
+        }
     }
 }
