d3eca1c5d8f638660f2a07391f21feb3ed6b87c1
Carlos Ferreira <Carlos Ferreira>
2017-01-24 16:11:19 -0500

    credit card and pax signature reprint

--- TabletCR/src/main/java/com/kaching123/tcr/fragment/tendering/payment/PayPrintAndFinishFragmentDialog.java
+++ TabletCR/src/main/java/com/kaching123/tcr/fragment/tendering/payment/PayPrintAndFinishFragmentDialog.java
@@ -303,9 +303,9 @@ public class PayPrintAndFinishFragmentDialog extends PrintAndFinishFragmentDialo
 
     private void printSignatureOrder(boolean skipPaperWarning, boolean searchByMac, ReceiptType receiptType, PrintSignatureOrderCallback printSignatureCallback) {
         //WaitDialogFragment.show(getActivity(), getString(R.string.wait_printing));
-        //if (receiptType != ReceiptType.DEBIT && receiptType != ReceiptType.EBT_CASH && receiptType != ReceiptType.EBT) {
+        if (receiptType != ReceiptType.DEBIT && receiptType != ReceiptType.EBT_CASH && receiptType != ReceiptType.EBT) {
             PrintSignatureOrderCommand.start(getActivity(), skipPaperWarning || this.ignorePaperEnd, searchByMac, orderGuid, transactions, receiptType, printSignatureCallback);
-       //}
+        }
     }
 
     boolean printed = false;
