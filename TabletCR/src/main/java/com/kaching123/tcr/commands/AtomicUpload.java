6ff98597ef917a91956e0e2e9f0433d6a08db464
CarlosEduardoFerreira <ferreira.carlos.e@gmail.com>
2017-03-29 13:10:33 -0400

    Fix to verify internet connection on Atomic Upload class

--- TabletCR/src/main/java/com/kaching123/tcr/commands/AtomicUpload.java
+++ TabletCR/src/main/java/com/kaching123/tcr/commands/AtomicUpload.java
@@ -2,7 +2,10 @@ package com.kaching123.tcr.commands;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.util.Log;
+import android.widget.Toast;
 
 import com.kaching123.tcr.TcrApplication;
 import com.kaching123.tcr.service.BatchSqlCommand;
@@ -26,33 +29,56 @@ public class AtomicUpload {
         DATA, EMPLOYEE
     }
 
+    Context context;
+
     public void upload(BatchSqlCommand sql, UploadType type){
         upload(sql, type, UploadObject.DATA);
     }
 
     public void upload(BatchSqlCommand sql, UploadType type, UploadObject object) {
 
-        Context context = TcrApplication.get().getApplicationContext();
+        context = TcrApplication.get().getApplicationContext();
 
-        if(type.equals(UploadType.LOCAL) || type.equals(UploadType.BOTH)) {
-            Log.d("BemaCarl7", "AtomicUpload.upload.type|sql1: " + type + "|" + sql.toJson());
-            ContentValues values = getContentValues(sql, System.currentTimeMillis(), true);
-            context.getContentResolver().insert(ShopProvider.contentUri(ShopStore.SqlCommandHostTable.URI_CONTENT), values);
+        if(hasInternetConnection()) {
 
-        }
+            if (type.equals(UploadType.LOCAL) || type.equals(UploadType.BOTH)) {
+                Log.d("BemaCarl7", "AtomicUpload.upload.type|sql1: " + type + "|" + sql.toJson());
+                ContentValues values = getContentValues(sql, System.currentTimeMillis(), true);
+                context.getContentResolver().insert(ShopProvider.contentUri(ShopStore.SqlCommandHostTable.URI_CONTENT), values);
 
-        if(type.equals(UploadType.WEB) || type.equals(UploadType.BOTH)) {
-            Log.d("BemaCarl7", "AtomicUpload.upload.type|sql2: " + type + "|" + sql.toJson());
-            ContentValues values = getContentValues(sql, System.currentTimeMillis(), false);
-            context.getContentResolver().insert(ShopProvider.contentUri(ShopStore.SqlCommandTable.URI_CONTENT), values);
-
-            if(object.equals(UploadObject.DATA)) {
-                OfflineCommandsService.startUpload(context);
-            }else{
-                OfflineCommandsService.startemployeeTableUpload(context);
             }
+
+            if (type.equals(UploadType.WEB) || type.equals(UploadType.BOTH)) {
+                Log.d("BemaCarl7", "AtomicUpload.upload.type|sql2: " + type + "|" + sql.toJson());
+                ContentValues values = getContentValues(sql, System.currentTimeMillis(), false);
+                context.getContentResolver().insert(ShopProvider.contentUri(ShopStore.SqlCommandTable.URI_CONTENT), values);
+
+                if (object.equals(UploadObject.DATA)) {
+                    OfflineCommandsService.startUpload(context);
+                } else {
+                    OfflineCommandsService.startemployeeTableUpload(context);
+                }
+            }
+
         }
 
+    }
+
+    public boolean hasInternetConnection(){
+
+        ConnectivityManager cm = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null && activeNetwork.isConnected();
+        if(isConnected) {
+            Log.d("BemaCarl7", "AtomicUpload.hasInternetConnection: Internet is connected.");
+            //Toast.makeText(context,"Internet is connected",Toast.LENGTH_SHORT).show();
+        }else {
+            Log.d("BemaCarl7", "AtomicUpload.hasInternetConnection: Internet is not connected.");
+            //Toast.makeText(context, "Internet is not connected, please check your settings", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        return true;
     }
 
 
